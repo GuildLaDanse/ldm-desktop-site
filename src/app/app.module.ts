4ea2362d4c14936af10f81acd5528712133b455a
Bavo De Ridder <bavo@bavoderidder.com>
2020-02-15 21:45:35 +0100

    Added ng-bootstrap and menu skeleton components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTileComponent } from './menu/menu-tile/menu-tile.component';
+import { MenuContainerComponent } from './menu/menu-container/menu-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTileComponent,
+    MenuContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu-tile/menu-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-tile',
+  templateUrl: './menu-tile.component.html',
+  styleUrls: ['./menu-tile.component.scss']
+})
+export class MenuTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
